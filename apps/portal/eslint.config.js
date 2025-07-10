9920502f9c8bc3a3c9d308b4954745f2bb2cd0a1
melwin <114172287+itzTedx@users.noreply.github.com>
2025-07-10 12:51:59 +0400

    Add '@ziron/db' dependency to portal application and update ESLint configuration. Refactored import statements in ESLint config, modified package.json to include '@ziron/db', and adjusted export path in '@ziron/auth' package.json for improved structure.

--- apps/portal/eslint.config.js
+++ apps/portal/eslint.config.js
@@ -1,6 +1,5 @@
-import baseConfig, { restrictEnvAccess } from "@ziron/eslint-config/base";
+import { restrictEnvAccess } from "@ziron/eslint-config/base";
 import nextjsConfig from "@ziron/eslint-config/nextjs";
-import reactConfig from "@ziron/eslint-config/react";
 
 /** @type {import('typescript-eslint').Config} */
 export default [
@@ -12,8 +11,8 @@ export default [
       "@typescript-eslint/no-unsafe-member-access": "off",
     },
   },
-  ...baseConfig,
-  ...reactConfig,
+  // ...baseConfig,
+  // ...reactConfig,
   ...nextjsConfig,
   ...restrictEnvAccess,
 ];
