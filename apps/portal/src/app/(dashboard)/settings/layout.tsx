fddbfaed16b598002e7144cddef65f73646f5078
melwin <114172287+itzTedx@users.noreply.github.com>
2025-07-25 03:30:16 +0400

    feat: Implement user authentication flow with login and registration components. Enhance form validation and error handling for improved user experience.

---
+++ apps/portal/src/app/(dashboard)/settings/layout.tsx
@@ -0,0 +1,127 @@
+import { ReactNode } from "react";
+import Link from "next/link";
+import { PageHeader } from "@/components/layout/page-header";
+import {
+  IconBell,
+  IconBuildingStore,
+  IconCreditCard,
+  IconShoppingBag,
+  IconUserEdit,
+} from "@tabler/icons-react";
+import { HouseIcon, SettingsIcon } from "lucide-react";
+
+import { ScrollArea, ScrollBar } from "@ziron/ui/components/scroll-area";
+import { Tabs, TabsList, TabsTrigger } from "@ziron/ui/components/tabs";
+
+export default async function SettingsLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <main className="mx-auto w-full max-w-7xl flex-1 pb-6">
+      <PageHeader title="Settings" />
+      <Tabs defaultValue="settings" className="w-full px-6" query={false}>
+        <ScrollArea>
+          <TabsList className="text-foreground mb-3 h-auto gap-2 rounded-none border-b bg-transparent px-0 py-1">
+            <Link href="/settings">
+              <TabsTrigger
+                value="settings"
+                className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+              >
+                <HouseIcon
+                  className="-ms-0.5 me-1 opacity-60"
+                  size={16}
+                  aria-hidden="true"
+                />
+                General
+              </TabsTrigger>
+            </Link>
+            <Link href="/settings/profile">
+              <TabsTrigger
+                value="profile"
+                className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+              >
+                <IconUserEdit
+                  className="-ms-0.5 me-1 opacity-60"
+                  size={16}
+                  aria-hidden="true"
+                />
+                Profile & Security
+              </TabsTrigger>
+            </Link>
+
+            <Link href="/settings/store">
+              <TabsTrigger
+                value="store"
+                className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+              >
+                <IconBuildingStore
+                  className="-ms-0.5 me-1 opacity-60"
+                  size={16}
+                  aria-hidden="true"
+                />
+                Business Info
+              </TabsTrigger>
+            </Link>
+
+            <Link href="/settings/payout">
+              <TabsTrigger
+                value="payout"
+                className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+              >
+                <IconCreditCard
+                  className="-ms-0.5 me-1 opacity-60"
+                  size={16}
+                  aria-hidden="true"
+                />
+                Payout & Bank Details
+              </TabsTrigger>
+            </Link>
+
+            <Link href="/settings/storefront">
+              <TabsTrigger
+                value="storefront"
+                className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+              >
+                <IconShoppingBag
+                  className="-ms-0.5 me-1 opacity-60"
+                  size={16}
+                  aria-hidden="true"
+                />
+                Storefront
+              </TabsTrigger>
+            </Link>
+
+            <Link href="/settings/notifications">
+              <TabsTrigger
+                value="notifications"
+                className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+              >
+                <IconBell
+                  className="-ms-0.5 me-1 opacity-60"
+                  size={16}
+                  aria-hidden="true"
+                />
+                Notifications
+              </TabsTrigger>
+            </Link>
+            <TabsTrigger
+              value="tab-6"
+              className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+            >
+              <SettingsIcon
+                className="-ms-0.5 me-1 opacity-60"
+                size={16}
+                aria-hidden="true"
+              />
+              Settings
+            </TabsTrigger>
+          </TabsList>
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
+        {children}
+      </Tabs>
+    </main>
+  );
+}
