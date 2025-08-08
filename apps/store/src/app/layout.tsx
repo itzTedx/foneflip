5304860b46cd2d5d07a7f66b3a60c1bbbeb2e126
melwin <114172287+itzTedx@users.noreply.github.com>
2025-08-08 10:23:04 +0400

    Update layout and Navbar components for improved UI; set HTML class to "dark" for theme support and enhance Navbar with logo, search, and sign-in button for better navigation experience.

--- apps/store/src/app/layout.tsx
+++ apps/store/src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html className="dark" lang="en">
       <body className={cn("antialiased", geistSans.className)}>
         <Navbar />
         {children}
--- apps/store/src/components/layout/navbar.tsx
+++ apps/store/src/components/layout/navbar.tsx
@@ -1,3 +1,23 @@
+import Link from "next/link";
+
+import { Logo } from "@ziron/ui/assets/logo";
+import { Button } from "@ziron/ui/button";
+
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="divide-y bg-background/80 backdrop-blur-xl">
+      <div className="container mx-auto flex items-center justify-between gap-2 py-2">
+        <Link href="/">
+          <Logo />
+        </Link>
+        <div>Search</div>
+        <div>
+          <Button asChild>
+            <Link href="/auth/login">Sign in</Link>
+          </Button>
+        </div>
+      </div>
+      <div className="container mx-auto flex items-center gap-4 py-2">Categoreis bar</div>
+    </nav>
+  );
 };
